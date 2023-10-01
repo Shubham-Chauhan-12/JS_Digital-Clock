5b8007fbe4d28bf031a3e0d316ebf51b04facc86
Shubham Chauhan <shubhamchauhan1204.99@gmail.com>
2023-10-01 16:59:07 +0530

    current time get by Date( )

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const hours = document.getElementById('#hours');
 const minutes = document.getElementById('#minutes');
 const seconds = document.getElementById('#seconds');
 const period = document.getElementById('#period');
+
+
+const currenthours = new Date().getHours();
+const currentminutes = new Date().getMinutes();
+const currentseconds = new Date().getSeconds();
+
+
+const currentZone = currenthours >= 12 ? "PM" : "AM";
